8681f5189db6849e5dfa0e2f3f1dd8234ba0ad73
liangjinyong <liangjy881@gmail.com>
2025-01-22 11:16:33 +0900

    04 编写App组件

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import './assets/main.css'
+// 引入createApp用于创建应用
+import { createApp } from "vue";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+// 引入App根组件
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
