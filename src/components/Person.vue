9214bf36a8805e4df7cbf01c52f88b1ec874dae8
liangjinyong <liangjy881@gmail.com>
2025-01-22 14:36:36 +0900

    10 setup的语法糖

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,59 +2,32 @@
   <div class="person">
     <h2>姓名: {{ name }}</h2>
     <h2>年龄: {{ age }}</h2>
+    <h2>地址: {{ address }}</h2>
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年龄</button>
     <button @click="showTel">查看联系方式</button>
-    <hr />
-    <h2>测试1: {{ a }}</h2>
-    <h2>测试2: {{ c }}</h2>
-    <h2>测试3: {{ d }}</h2>
-    <button @click="b">测试</button>
   </div>
 </template>
 
-<script lang="ts">
-export default {
-  name: "Person",
-  beforeCreate() {
-    console.log("beforeCreate");
-  },
-  data() {
-    return {
-      a: 100,
-      c: this.name,
-      d: 900,
-    };
-  },
-  methods: {
-    b() {
-      console.log("b");
-    },
-  },
-  setup() {
-    // 数据，原来是写在data中的，此时的name, age, tel都不是响应式的数据
-    let name = "张三";
-    let age = 18;
-    let tel = "13888888888";
-    // let x = this.d;
+<script lang="ts" setup name="Person">
+// 数据，原来是写在data中的，此时的name, age, tel都不是响应式的数据
+let name = "张三";
+let age = 18;
+let tel = "13888888888";
+let address = "北京昌平区宏福苑 宏福科技园";
 
-    // 方法
-    function changeName() {
-      name = "zhang-san"; // 注意: 这样修改name，页面是没有变化的
-      console.log(name); // name确实修改了，但name不是响应式的
-    }
-    function changeAge() {
-      age++; // 注意: 这样修改age，页面是没有变化的
-      console.log(age); // age确实修改了，但age不是响应式的
-    }
-    function showTel() {
-      alert(tel);
-    }
-
-    // 将数据，方法交出去，模版中才可以使用
-    return { name, age, changeName, changeAge, showTel };
-  },
-};
+// 方法
+function changeName() {
+  name = "zhang-san"; // 注意: 这样修改name，页面是没有变化的
+  console.log(name); // name确实修改了，但name不是响应式的
+}
+function changeAge() {
+  age++; // 注意: 这样修改age，页面是没有变化的
+  console.log(age); // age确实修改了，但age不是响应式的
+}
+function showTel() {
+  alert(tel);
+}
 </script>
 
 <style>
