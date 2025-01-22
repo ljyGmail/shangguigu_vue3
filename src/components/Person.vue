cba43ca9f373942726c38e1f654fdb3ba0b9466b
liangjinyong <liangjy881@gmail.com>
2025-01-22 15:41:14 +0900

    13 ref创建 对象类型的响应式数据

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="person">
-    <h2>一辆{{ car.brand }}车，价值{{ car.price }}万</h2>
+    <h2>汽车信息: 一辆{{ car.brand }}车，价值{{ car.price }}万</h2>
     <button @click="changePrice">修改汽车的价格</button>
     <br />
     <h2>游戏列表:</h2>
@@ -8,50 +8,36 @@
       <li v-for="g in games" :key="g.id">{{ g.name }}</li>
     </ul>
     <button @click="changeFirstGame">修改第一个游戏的名字</button>
-    <hr />
-    <h2>测试: {{ obj.a.b.c }}</h2>
-    <button @click="changeObj">测试</button>
   </div>
 </template>
 
 <script lang="ts" setup name="Person">
-import { reactive } from "vue";
+import { ref, reactive } from "vue";
 
 // 数据
-let car = reactive({ brand: "奔驰", price: 100 });
-// console.log(car);
-// console.log(Proxy);
+let car = ref({ brand: "奔驰", price: 100 });
 
-let games = reactive([
+let games = ref([
   { id: "game01", name: "王者荣耀" },
   { id: "game02", name: "元神" },
   { id: "game03", name: "三国志" },
 ]);
 
-let obj = reactive({
-  a: {
-    b: {
-      c: 666,
-    },
-  },
-});
+let obj = reactive({ x: 999 });
+
+// let name = reactive("张三");
 
 console.log(car);
-console.log(games);
 console.log(obj);
 
 // 方法
 function changePrice() {
-  car.price += 10;
-  console.log(car.price);
+  car.value.price += 10;
+  console.log(car.value.price);
 }
 
 function changeFirstGame() {
-  games[0].name = "流星蝴蝶剑";
-}
-
-function changeObj() {
-  obj.a.b.c = 999;
+  games.value[0].name = "流星蝴蝶剑";
 }
 </script>
 
