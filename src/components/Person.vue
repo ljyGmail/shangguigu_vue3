0369f0f3217b2c21c6b5824be71bd6d8f2a31aab
liangjinyong <liangjy881@gmail.com>
2025-01-22 17:00:35 +0900

    16 computed计算属性

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,41 +1,66 @@
 <template>
   <div class="person">
-    <h2>姓名: {{ name }}</h2>
-    <h2>年龄: {{ age }}, {{ nl }}</h2>
-    <button @click="changeName">修改名字</button>
-    <button @click="changeAge">修改年龄</button>
+    姓: <input type="text" v-model="firstName" /><br />
+    名: <input type="text" v-model="lastName" /><br />
+    <button @click="changeFullName">将全名改为li-si</button><br />
+    全名: <span>{{ fullName }}</span ><br />
+    全名: <span>{{ fullName }}</span ><br />
   </div>
 </template>
 
 <script lang="ts" setup name="Person">
-import { reactive, toRefs, toRef } from "vue";
+import { ref, computed } from "vue";
 
-// 数据
-let person = reactive({
-  name: "张三",
-  age: 18,
+let firstName = ref("zhang");
+let lastName = ref("san");
+
+// 这么定义的fullName是一个计算属性，且是只读的
+/*
+let fullName = computed(() => {
+  console.log(1); // 计算属性有缓存
+  return (
+    firstName.value.slice(0, 1).toUpperCase() +
+    firstName.value.slice(1) +
+    "-" +
+    lastName.value
+  );
+});
+*/
+
+// 这么定义的fullName是一个计算属性，可读可写
+let fullName = computed({
+  get() {
+    return (
+      firstName.value.slice(0, 1).toUpperCase() +
+      firstName.value.slice(1) +
+      "-" +
+      lastName.value
+    );
+  },
+  set(val) {
+    const [str1, str2] = val.split("-");
+    firstName.value = str1;
+    lastName.value = str2;
+  },
 });
 
-let { name, age } = toRefs(person);
-// console.log(name, age);
-
-let nl = toRef(person, "age");
-console.log(nl, nl.value);
-
-let x = toRefs(person);
-console.log(x);
-
-// 方法
-function changeName() {
-  name.value += "~";
-  console.log(name.value, person.name);
+function changeFullName() {
+  console.log(fullName.value);
+  fullName.value = "li-si";
 }
 
-function changeAge() {
-  age.value += 1;
-  console.log(age);
+function fullName2() {
+  console.log(1); // 方法没有缓存
+
+  return (
+    firstName.value.slice(0, 1).toUpperCase() +
+    firstName.value.slice(1) +
+    "-" +
+    lastName.value
+  );
 }
 </script>
+
 <style>
 .person {
   background-color: skyblue;
