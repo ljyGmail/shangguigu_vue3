47353c221f997e497a4dc6da0386852dcb507f1c
liangjinyong <liangjy881@gmail.com>
2025-01-23 23:40:16 +0900

    51 store组合式写法

--- src/store/loveTalk.ts
+++ src/store/loveTalk.ts
@@ -1,7 +1,9 @@
 import { defineStore } from "pinia";
 import axios from "axios";
 import { nanoid } from "nanoid";
+import { reactive } from "vue";
 
+/*
 export const useTalkStore = defineStore("talk", {
   actions: {
     async getATalk() {
@@ -19,4 +21,22 @@ export const useTalkStore = defineStore("talk", {
       talkList: JSON.parse(localStorage.getItem("talkList") as string) || [],
     };
   },
+});
+*/
+
+export const useTalkStore = defineStore("talk", () => {
+  const talkList = reactive(
+    JSON.parse(localStorage.getItem("talkList") as string) || []
+  );
+  // getATalk函数相当于action
+  async function getATalk() {
+    let {
+      data: { content: title },
+    } = await axios.get("https://api.uomg.com/api/rand.qinghua?format=json");
+    // 把请求回来的字符串, 包装成一个对象
+    let obj = { id: nanoid(), title };
+    talkList.unshift(obj);
+  }
+
+  return { talkList, getATalk };
 });
