f9f686bca087f5cf3d7ed787a251f4b9a46b81a3
liangjinyong <liangjy881@gmail.com>
2025-01-23 20:08:07 +0900

    47 修改数据的三种方式

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,10 +1,21 @@
 import { defineStore } from "pinia";
 
 export const useCountStore = defineStore("count", {
+  // actions里面放置的是一个一个的动作方法, 用于响应组件中的"动作"
+  actions: {
+    increment(value) {
+      if (this.sum < 10) {
+        // 修改数据
+        this.sum += value;
+      }
+    },
+  },
   // 真正存储数据的地方
   state() {
     return {
       sum: 6,
+      school: "atguigu",
+      address: "宏福科技园",
     };
   },
 });
