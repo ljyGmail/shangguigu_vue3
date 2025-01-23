8b36748e5781f224fd3249b1ddb4ff8a73d5e6bd
liangjinyong <liangjy881@gmail.com>
2025-01-23 17:05:29 +0900

    45 搭建pinia环境

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-// 引入createApp用于创建应用
 import { createApp } from "vue";
-
-// 引入App根组件
 import App from "./App.vue";
-// 引入路由器
+// 第一步: 引入pinia
+import { createPinia } from "pinia";
 
-// 创建一个应用
 const app = createApp(App);
-// 挂载整个应用到app容器中
+
+// 第二步: 创建pinia
+const pinia = createPinia();
+
+// 第三步: 安装pinia
+app.use(pinia);
+
 app.mount("#app");
