98b991b13a8cfa1210e1192627764b63408a743e
liangjinyong <liangjy881@gmail.com>
2025-01-23 13:56:45 +0900

    36 路由 嵌套路由

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -17,10 +17,12 @@ onUnmounted(() => {
 </script>
 
 <style scoped>
-.home {
+.about {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100%;
+  color: rgb(85, 84, 84);
+  font-size: 18px;
 }
 </style>
--- src/pages/News.vue
+++ src/pages/News.vue
@@ -1,21 +1,57 @@
 <template>
   <div class="news">
+    <!-- 导航区 -->
     <ul>
-      <li><a href="#">新闻001</a></li>
-      <li><a href="#">新闻002</a></li>
-      <li><a href="#">新闻003</a></li>
-      <li><a href="#">新闻004</a></li>
+      <li v-for="news in newsList" :key="news.id">
+        <RouterLink to="/news/detail">{{ news.title }}</RouterLink>
+      </li>
     </ul>
+    <!-- 展示区 -->
+    <div class="news-content">
+      <RouterView></RouterView>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts" name="News"></script>
+<script setup lang="ts" name="News">
+import { reactive } from "vue";
+import { RouterView, RouterLink } from "vue-router";
+
+const newsList = reactive([
+  { id: "id01", title: "一种抗癌食物", content: "西兰花" },
+  { id: "id02", title: "如何一夜暴富", content: "学IT" },
+  { id: "id03", title: "震惊，万万没想到", content: "明天是周一" },
+  { id: "id04", title: "好消息! 好消息!", content: "快过年了" },
+]);
+</script>
 
 <style scoped>
-.home {
+.news {
+  padding: 0 20px;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-between;
   height: 100%;
+}
+
+.news ul {
+  margin-top: 30px;
+  list-style: none;
+  padding-left: 10px;
+}
+
+.news li > a {
+  font-size: 18px;
+  line-height: 40px;
+  text-decoration: none;
+  color: #64967e;
+  text-shadow: 0 0 1px rgb(0, 84, 0);
+}
+
+.news-content {
+  width: 70%;
+  height: 90%;
+  border: 1px solid;
+  margin-top: 20px;
+  border-radius: 10px;
 }
 </style>
