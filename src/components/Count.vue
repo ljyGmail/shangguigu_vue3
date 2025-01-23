f8e5bcd0111eb660f8c64f345d5fd4486554171c
liangjinyong <liangjy881@gmail.com>
2025-01-23 19:50:04 +0900

    46 存储+读取数据

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="count">
-    <h2>当前求和为: {{ sum }}</h2>
+    <h2>当前求和为: {{ countStore.sum }}</h2>
     <select v-model.number="n">
       <option value="1">1</option>
       <option value="2">2</option>
@@ -12,18 +12,38 @@
 </template>
 
 <script setup lang="ts" name="Count">
-import { ref } from "vue";
+import { ref, reactive } from "vue";
+import { useCountStore } from "@/store/count";
+
+const countStore = useCountStore();
+// 以下两种方式都可以拿到state中的数据
+console.log("@@@", countStore.sum);
+console.log("@@@", countStore.$state.sum);
 
 // 数据
-let sum = ref(1); // 当前求和
 let n = ref(1); // 用户选择的数字
 
+/*
+let obj = reactive({
+  a: 1,
+  b: 2,
+  c: ref(3),
+});
+
+console.log(obj.a);
+console.log(obj.b);
+console.log(obj.c);
+
+let x = ref(9);
+console.log(x.value);
+*/
+
 function add() {
-  sum.value += n.value;
+  // sum.value += n.value;
 }
 
 function minus() {
-  sum.value -= n.value;
+  // sum.value -= n.value;
 }
 </script>
 
