d736d6102f067bfdb5f5bab0dd2c07d763fd7515
liangjinyong <liangjy881@gmail.com>
2025-01-23 13:21:49 +0900

    33 路由 路由器工作模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,12 @@
 // 创建一个路由器，并暴露出去
 
 // 第一步: 引入createRouter
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
+
 // 引入一个一个可能要呈现的组件
 import Home from "@/pages/Home.vue";
 import News from "@/pages/News.vue";
@@ -9,7 +14,7 @@ import About from "@/pages/About.vue";
 
 // 第二步: 创建路由器
 const router = createRouter({
-  history: createWebHistory(), // 路由器的工作模式
+  history: createWebHashHistory(), // 路由器的工作模式
   routes: [
     // 一个一个点路由规则
     {
