c0e78e58f24d4f08539269cb63c5603598b63f92
liangjinyong <liangjy881@gmail.com>
2025-01-23 10:19:28 +0900

    31 路由 基本切换效果

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h2>大家好，欢迎来到尚硅谷直播间</h2>
+  </div>
+</template>
+
+<script setup lang="ts" name="About"></script>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+</style>
---
+++ src/components/News.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="news">
+    <ul>
+      <li><a href="#">新闻001</a></li>
+      <li><a href="#">新闻002</a></li>
+      <li><a href="#">新闻003</a></li>
+      <li><a href="#">新闻004</a></li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts" name="News"></script>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+</style>
