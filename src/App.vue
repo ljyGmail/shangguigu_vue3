3f205e66f2fc1c9d66b12fd4bec7024f1a56c159
liangjinyong <liangjy881@gmail.com>
2025-01-24 13:01:36 +0900

    52 组件通信 方式1 props

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <template>
-  <Count />
-  <br />
-  <LoveTalk />
+  <div class="container-fluid wrapper">
+    <h1 class="title">Vue3 组件通信</h1>
+    <hr />
+    <div class="row">
+      <div class="col-xs-3 col-md-3 col-lg-3 col-xl-3">
+        <!-- 导航区 -->
+        <router-link active-class="active" class="list-group-item" to="/props"
+          >1. props</router-link
+        >
+      </div>
+      <div class="col-xs-9 col-md-9 col-lg-9 col-xl-9">
+        <div class="panel-body">
+          <!-- 占位一个展示区 -->
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts" name="App">
-import Count from "./components/Count.vue";
-import LoveTalk from "./components/LoveTalk.vue";
-</script>
+<script setup lang="ts" name="App"></script>
 
-<style scoped></style>
+<style scoped>
+.wrapper .title {
+  padding: 20px;
+  text-align: center;
+  min-width: 610px;
+}
+
+.wrapper .small {
+  font-size: 15px;
+}
+
+.wrapper .list-group-item {
+  min-width: 230px;
+}
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Props from "@/pages/01_props/Father.vue";
+
+export default createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/props",
+      component: Props,
+    },
+    {
+        path:'/',
+        redirect:'/props'
+    }
+  ],
+});
